9bdfcd0bb9df4e7e64e5d7dfb86f9cb2a8bf127c
Vladimir <52083020+VDT-26@users.noreply.github.com>
2026-03-20 11:12:07 +0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (92.74%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -3,3 +3,26 @@ class Solution:
         set1 = set(nums1)
         set2 = set(nums2)
         return list(set1 & set2) #автоматом выбираем пересечения ._.
+
+"""
+Я бы так сделал, но неэффективно. Надо ззапомнить первый вариант
+class Solution:
+    def intersection(self, nums1, nums2):
+        seen = set()
+        duplicates = []
+
+        for x in nums1:
+            if x in seen:
+                duplicates.append(x)   # повтор — отправляем в список
+            else:
+                seen.add(x)
+
+        result = []
+        set2 = set(nums2)
+
+        for x in seen:
+            if x in set2:
+                result.append(x)
+
+        return result
+"""
