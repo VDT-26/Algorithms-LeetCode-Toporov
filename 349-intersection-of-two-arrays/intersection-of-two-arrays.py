bc22b3c03cc81c4c3ae2dc440b26bf81bafaed72
Vladimir <52083020+VDT-26@users.noreply.github.com>
2026-03-20 11:09:17 +0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (71.84%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -2,4 +2,4 @@ class Solution:
     def intersection(self, nums1, nums2):
         set1 = set(nums1)
         set2 = set(nums2)
-        return list(set1 & set2)
+        return list(set1 & set2) #автоматом выбираем пересечения ._.
