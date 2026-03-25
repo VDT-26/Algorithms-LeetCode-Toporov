2c45a1a34a25dd09826c93da1b7c3ec1f9d3a624
Vladimir <52083020+VDT-26@users.noreply.github.com>
2026-03-25 10:54:18 +0500

    Time: 0 ms (100.00%) | Memory: 19.2 MB (96.54%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -4,8 +4,8 @@ class Solution:
         def search(node): 
             if not node: # Если пусто -- выходим
                 return 0
-            search(node.left)   # Идем влево
-            answer.append(node.val) # Добавляем значение в ответный список
-            search(node.right)  # Идём вправо
-        search(root)    # Идём наверх
+            search(node.left)   # Идем влево до последнего
+            answer.append(node.val) # Левое поддерево уже обработано -- можно записать значение текущего узла
+            search(node.right)  # Идём вправо до последнего
+        search(root)    # Начинаем с корня
         return answer
